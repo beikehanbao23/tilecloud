fa37d46d349240469db08c4319aeeb1381dd8dc7
huzhenhui <>
2018-07-06 08:35:14 +0800

    minedata 添加

---
+++ tiles/minedata_com.py
@@ -0,0 +1,7 @@
+from tilecloud.layout.template import TemplateTileLayout
+from tilecloud.store.url import URLTileStore
+
+
+tilestore = URLTileStore(
+    (TemplateTileLayout('http://datahive.minedata.cn/mergeddata/Villtown,Road,Railway,Ptline,Adminflag,Poi,Annotation,Worldannotation/%(z)d/%(x)d/%(y)d.png?token=449ba822788c46bea9f90dfba48e5269&solu=4024'.format(server)) for server in 'abc'),
+    attribution='&copy; Minedata contributors, willmap', content_type='image/pbf')
